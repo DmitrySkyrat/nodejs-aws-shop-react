acc3581d4fa9182a108a0830895226a5cd053fc1
Dmitriy <dmitriy@Macbook-KR7X4L2FQY.local>
2026-06-19 10:48:59 -0700

    task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://irrr6kcq8l.execute-api.us-east-1.amazonaws.com/prod/api/profile/cart",
   import: "https://em611gf3ff.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://irrr6kcq8l.execute-api.us-east-1.amazonaws.com/prod/api",
+  cart: "http://DmitrySkyrat-cart-api-develop.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
