7778b9fda9025acd7b80b5627ad85a683bd70398
Dmitriy <dmitriy@Macbook-KR7X4L2FQY.local>
2026-06-21 03:51:38 -0700

    task-10

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://yplau1qv4d.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://irrr6kcq8l.execute-api.us-east-1.amazonaws.com/prod/api/profile/cart",
   import: "https://em611gf3ff.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://DmitrySkyrat-bff-api-prod.us-east-1.elasticbeanstalk.com",
   cart: "http://DmitrySkyrat-cart-api-develop.us-east-1.elasticbeanstalk.com/api",
 };
 
